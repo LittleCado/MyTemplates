77f8a5301f1597472f9b52a2da584b3971c115f1
Viacheslav <little.cado1@gmail.com>
2020-08-25 23:14:49 +0300

    Add sidebar menu

---
+++ sidebar_menu/index.js
@@ -0,0 +1,6 @@
+function toggleMenu () {
+    const navigation = document.querySelector('.navigation');
+    const toggle = document.querySelector('.toggle');
+    navigation.classList.toggle('active');
+    toggle.classList.toggle('active')
+}
